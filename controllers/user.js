20007f24d79d7fe997bceebfb7ee1c5bc3f7938b
Albert Lazerson <albert@lazerson-design.de>
2024-10-02 15:45:37 +0200

    Implemented User controller, model, and routes

---
+++ controllers/user.js
@@ -0,0 +1,73 @@
+//Get all users
+
+const User = require("../models/User");
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+//Post a new user
+const createUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const newUser = await User.create({ name, email, password });
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+//Get user by ID
+const getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+//Put Update a user
+const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, email } = req.body;
+    const user = await User.findByPk(id);
+    if (user) {
+      user.name = name || user.name;
+      user.email = email || user.email;
+      await user.save();
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+//Delete a user
+const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    if (user) {
+      await user.destroy();
+      res.status(200).json({ message: "User deleted successfully" });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
