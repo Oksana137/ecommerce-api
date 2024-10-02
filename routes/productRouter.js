b6769c4baba0d842de6ad115a2c50d1958c90744
Albert Lazerson <albert@lazerson-design.de>
2024-10-02 16:31:33 +0200

    updated product controller, productRouter, index updated with product and route

---
+++ routes/productRouter.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  getProducts,
+  createProduct,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/product.js";
+
+const router = express.Router();
+
+router.get("/products", getProducts);
+router.post("/products", createProduct);
+router.get("/products/:id", getProductById);
+router.put("/products/:id", updateProduct);
+router.delete("/products/:id", deleteProduct);
+
+export default router;
